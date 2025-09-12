8278325c6ab9d41d0d0b0a43940575144435c6bf
Minh Khang <khangtm@imt-soft.com>
2025-09-12 23:19:49 +0700

    Update Dependencies.cs

--- Game.Application/Dependencies.cs
+++ Game.Application/Dependencies.cs
@@ -4,9 +4,9 @@ using Game.Application.Infra.DataAccess;
 using Game.Application.Infra.DataAccess.UnitOfWork;
 using Game.Application.Infra.Http;
 using Game.Application.Infra.Logging;
-using Game.Application.Infra.Realtime;
 using Game.Application.Interface;
 using Game.Application.Interface.DataAccess.UnitOfWork;
+using Game.Application.Interface.Realtime;
 using Game.Application.Service.Base.Locator;
 using Microsoft.EntityFrameworkCore;
 
